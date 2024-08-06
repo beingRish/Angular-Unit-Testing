f81d1ec21afcbd56b94fc4860704d82f7e0bb28e
beingRish <rishabhjuly11@gmail.com>
2024-08-05 22:37:51 -0700

    Practical implementation of DebugElement

--- src/app/components/post/post.component.spec.ts
+++ src/app/components/post/post.component.spec.ts
@@ -3,6 +3,7 @@ import { PostComponent } from "./post.component"
 import { first } from "rxjs";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
+import { By } from "@angular/platform-browser";
 
 describe('Post Component', () => {
 
@@ -30,6 +31,15 @@ describe('Post Component', () => {
         const postElement: HTMLElement = fixture.nativeElement;
         const a = postElement.querySelector('a')
         expect(a?.textContent).toContain(post.title);
+    })
+
+    it('should render the post title in the anchor element using debug element', () => {
+        const post: Post = {id: 1, body: 'body 1', title: 'title 1'}
+        comp.post = post;
+        fixture.detectChanges();
+        const postDebugElement = fixture.debugElement;
+        const aElement: HTMLElement = postDebugElement.query(By.css('a')).nativeElement;
+        expect(aElement?.textContent).toContain(post.title);
     })
 
 
