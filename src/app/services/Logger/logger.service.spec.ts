2bc48eb7ae8e27462b4dd250472ac66dd48e5f86
beingRish <rishabhjuly11@gmail.com>
2024-08-06 23:18:25 -0700

    Created instance for Services using inject method

--- src/app/services/Logger/logger.service.spec.ts
+++ src/app/services/Logger/logger.service.spec.ts
@@ -1,3 +1,4 @@
+import { TestBed } from "@angular/core/testing";
 import { LoggerService } from "./logger.service"
 
 
@@ -6,7 +7,11 @@ describe('LoggerService', () => {
   let service: LoggerService;
 
   beforeEach(() => {
-    service = new LoggerService()
+    TestBed.configureTestingModule({
+      providers: [LoggerService]
+    });
+    
+    service = TestBed.inject(LoggerService);
   })
 
   it('should not have any messages at starting', () => {
