cecd284fefc2c11058453040b5606bedaa96a421
beingRish <rishabhjuly11@gmail.com>
2024-08-07 00:48:19 -0700

    Testing multiple Http using HttpTestController

---
+++ src/app/services/Post/httpclient.testing.spec.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from "@angular/common/http"
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+import { TestBed } from "@angular/core/testing"
+
+let testUrl = '/data';
+interface Data {
+    name: string;
+}
+
+describe('Http Client Testing Module', () => {
+    let httpClient: HttpClient;
+    let httpTestingController: HttpTestingController;
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule]
+        })
+        httpClient = TestBed.inject(HttpClient);
+        httpTestingController = TestBed.inject(HttpTestingController)
+    })
+
+    it('should call the testUrl with get Request', () => {
+        const testData: Data = { name: 'Rishabh' };
+        httpClient.get<Data>(testUrl).subscribe(data => {
+            // expect(data).toEqual(testData);
+        });
+        const request = httpTestingController.expectOne('/data');
+        request.flush(testData);
+        expect(request.request.method).toBe('GET');
+    })
+})
